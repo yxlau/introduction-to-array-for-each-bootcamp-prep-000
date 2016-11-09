70e23ef07186e05d8b1cd50983b9502ab935102e
Yi-Xuan Lau <yixuan.lau@gmail.com>
2016-11-09 07:52:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,6 @@
+function changeCompletely(element, index, array) {
+  array[index] = (Math.random() * 100).toString() + '!!!'
+}
+function doToElementsInArray(array, callback) {
+  array.forEach(callback)
+}
